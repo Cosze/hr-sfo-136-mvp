b8ae40254bad47bb69b7aa63c047ef6950f754da
Cody Szeto <codyszet@gmail.com>
2021-08-25 15:29:08 -0700

    Denormalized db schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,20 +3,23 @@ CREATE DATABASE mvp;
 
 \c mvp;
 
-CREATE TABLE clients(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  room SMALLINT NOT NULL
-);
+-- CREATE TABLE clients(
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL,
+--   room SMALLINT NOT NULL
+-- );
 
-CREATE TABLE servers(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE servers(
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(30) NOT NULL
+-- );
 
 CREATE TABLE requests(
   id SERIAL PRIMARY KEY,
-  time BIGINT,
+  client_name VARCHAR(50) NOT NULL,
+  room SMALLINT NOT NULL,
+  server_name VARCHAR(50),
+  schedule BIGINT,
   time_accepted BIGINT,
   time_started BIGINT,
   time_completed BIGINT,
@@ -24,5 +27,5 @@ CREATE TABLE requests(
   tip SMALLINT
 );
 
-ALTER TABLE requests ADD FOREIGN KEY (client_id) REFERENCES clients(id);
-ALTER TABLE requests ADD FOREIGN KEY (server_id) REFERENCES servers(id);
+-- ALTER TABLE requests ADD FOREIGN KEY (client_id) REFERENCES clients(id);
+-- ALTER TABLE requests ADD FOREIGN KEY (server_id) REFERENCES servers(id);
