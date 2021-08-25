9509f48b578e4b9d79e5e70bcf0e724a1b8d72a8
Cody Szeto <codyszet@gmail.com>
2021-08-25 13:14:57 -0700

    Initiated model methods with comments detailing what each function should do

--- server/models/clients.js
+++ server/models/clients.js
@@ -1,3 +1,9 @@
 const db = require('../../db/database.js');
 
-module.exports = {};
+module.exports = {
+    login: (userDetails) => {}, // function that will confirm a login session
+    postRequest: (userID, data) => {}, // function that posts a request into the database
+    getRequests: (userID) => {}, // function that gets all requests made by this user
+    updateRequest: (userID, requestID) => {}, // function that edits a request that is either open (or accepted)
+    cancelRequest: (userID, requestID) => {}, // function that removes a request from database
+};
--- server/models/servers.js
+++ server/models/servers.js
@@ -1,3 +1,10 @@
 const db = require('../../db/database.js');
 
-module.exports = {};
+module.exports = {
+    getRequests: (status) => {}, // gets open requests from database, sorted by scheduled time
+    getCompleted: (serverID) => {}, // gets completed requests from database
+    acceptRequest: (requestID) => {}, // updates status of a request to accepted, updates server name and time accepted
+    startRequest: (requestID) => {}, // updates status of a request to started, sets request start time
+    completeRequest: (requestID) => {}, // updates status of a request to completed, sets request completed time
+    cancelRequest: (requestID) => {}, // updates status of a request to open, sets server name and time accepted to null
+};
