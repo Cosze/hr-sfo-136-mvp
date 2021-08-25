cd66d485181ec3e62ed25cf36f8c3f36b1976c39
Cody Szeto <codyszet@gmail.com>
2021-08-25 13:48:54 -0700

    Connected client controllers to client models

--- server/controllers/clients.js
+++ server/controllers/clients.js
@@ -1,9 +1,49 @@
 const models = require('../models');
 
 module.exports = {
-    login: (req, res) => {}, // login as client
-    postRequest: (req, res) => {}, // post new request
-    getRequests: (req, res) => {}, // get all requests client has made
-    updateRequest: (req, res) => {}, // update an open or started request
-    cancelRequest: (req, res) => {}, // cancel an open request
+    login: (req, res) => {
+        // login as client
+        try {
+            const data = await models.clients.login();
+            res.status(200).send(data);
+        } catch (e) {
+            res.status(500).send('Error logging in for client', e);
+        }
+    },
+    postRequest: (req, res) => {
+        // post new request
+        try {
+            const data = await models.clients.postRequest();
+            res.status(200).send(data);
+        } catch (e) {
+            res.status(500).send('Error posting request for client', e);
+        }
+    },
+    getRequests: (req, res) => {
+        // get all requests client has made
+        try {
+            const data = await models.clients.getRequests();
+            res.status(200).send(data);
+        } catch (e) {
+            res.status(500).send('Error getting requests for client', e);
+        }
+    },
+    updateRequest: (req, res) => {
+        // update an open or started request
+        try {
+            const data = await models.clients.updateRequest();
+            res.status(200).send(data);
+        } catch (e) {
+            res.status(500).send('Error updating request for client', e);
+        }
+    },
+    cancelRequest: (req, res) => {
+        // cancel an open request
+        try {
+            const data = await models.clients.cancelRequest();
+            res.status(200).send(data);
+        } catch (e) {
+            res.status(500).send('Error cancelling request for client', e);
+        }
+    },
 };
