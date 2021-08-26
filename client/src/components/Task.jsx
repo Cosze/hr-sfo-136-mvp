8b2fa9c4640c212cc96b62d95d428190f39bc456
Cody Szeto <codyszet@gmail.com>
2021-08-26 09:46:05 -0700

    Added modal screen

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { Taskbox, Status } from './Styled';
 
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
 const Task = ({tasks}) => {
+    let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    Modal.setAppElement(document.getElementById('app'));
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
     return (
         <div>
+            <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+             </Modal>
             {tasks[0].map((task, index) => {
                 let time = Number(task.schedule);
                 time = new Date(time);
                 time = time.toString();
                 time = time.split(' ').slice(0,5).join(' ');
-                return <Taskbox key={index} onClick={() => alert('open detail modal')}>{time}<Status>{task.status}</Status></Taskbox>;
+                return <Taskbox key={index} onClick={openModal}>{time}<Status>{task.status}</Status></Taskbox>;
             })}
         </div>
     );
