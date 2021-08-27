8ad69d4a63f3db9729f9299a1f2e398e5e9b586e
Cody Szeto <codyszet@gmail.com>
2021-08-26 22:11:42 -0700

    Added compression

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const compression = require('compression');
 const router = require('./routes.js');
 const app = express();
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(compression());
 app.use(express.static('client/dist'));
 
 app.get('/', (req, res) => {
