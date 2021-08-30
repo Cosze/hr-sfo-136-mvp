5bb33a2f6fd5bfdc744a6c3702da8192a06945d4
Cody Szeto <codyszet@gmail.com>
2021-08-30 03:15:26 -0700

    Added some new input fields for account creation

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Forms, Input, SignInButton } from './Styled';
 
 const Login = (props) => {
+    const [newAcc, setNewAcc] = useState(false);
     return (
         <Forms login>
+            {newAcc ? <Input type='text' name='name' placeholder='Role' login></Input> : null}
+            {newAcc ? <Input type='text' name='name' placeholder='Name' login></Input> : null}
             <Input type='text' name='username' placeholder='Username' login></Input>
             <Input type='password' name='password' placeholder='Password' login></Input>
-            <SignInButton onClick={ev => {
+            {newAcc ? null : <SignInButton onClick={ev => {
                 ev.preventDefault();
                 console.log('clicked!');
-            }}>SIGN IN</SignInButton>
+            }}>SIGN IN</SignInButton>}
             <SignInButton onClick={ev => {
                 ev.preventDefault();
-                console.log('clicked!');
+                setNewAcc(!newAcc);
             }}>CREATE ACCOUNT</SignInButton>
         </Forms>
     );
